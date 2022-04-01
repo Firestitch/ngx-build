f0510037b7c3fa5e4a8926751b270e5415e5cbd7
Eugene Kubesh <codbasters@gmail.com>
2022-04-01 16:03:30 +0300

    update to ng12

--- src/public_api.ts
+++ src/public_api.ts
@@ -6,7 +6,7 @@ export { FsBuildModule } from './app/fs-build.module';
 
 export { BuildConfig } from './app/interfaces/build-config';
 export { BuildData } from './app/interfaces/build-data';
-
+export { FsBuildComponent } from './app/components/build/build.component';
 export { FS_BUILD_CONFIG } from './app/injectors/build-config.injector';
 export { FS_BUILD_DATA } from './app/injectors/build-data.injector';
 
