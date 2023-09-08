eb2a9084517a9826ddb180be0870ad7c5b07ab7f
Raymond Gigliotti <ray@firestitch.com>
2023-09-08 06:57:55 -0400

    removed unused code

--- src/app/fs-build.module.ts
+++ src/app/fs-build.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon';
@@ -7,10 +7,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { FsPromptModule } from '@firestitch/prompt';
 
 import { FsBuildComponent } from './components/build/build.component';
-import { FsBuildService } from './services/build.service';
-import { BuildConfig } from './interfaces/build-config';
-import { FS_BUILD_CONFIG } from './injectors';
-import { CompareMethod, UpdateAction } from './enums';
+
 
 
 @NgModule({
@@ -28,27 +25,4 @@ import { CompareMethod, UpdateAction } from './enums';
   ]
 })
 export class FsBuildModule {
-  // static forRoot(config: BuildConfig = {}): ModuleWithProviders<FsBuildModule> {
-  //   const defaultConfig: BuildConfig = {
-  //     interval: 30,
-  //     path: 'assets/build.json',
-  //     origin: window.location.origin,
-  //     updateAction: UpdateAction.PromptUpdate,
-  //     compareMethod: CompareMethod.Date,
-  //   };
-
-  //   return {
-  //     ngModule: FsBuildModule,
-  //     providers: [
-  //       {
-  //         provide: FS_BUILD_CONFIG,
-  //         useValue: {
-  //           ...defaultConfig,
-  //           ...config,
-  //         },
-  //       },
-  //       FsBuildService,
-  //     ],
-  //   };
-  // }
 }
